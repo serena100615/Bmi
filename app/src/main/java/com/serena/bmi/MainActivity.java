99ba519efdc6041f6e2632fc9ab80abb5784064b
serena100615 <erena>
2017-10-24 12:10:06 +0800

    calculate bmi

--- app/src/main/java/com/serena/bmi/MainActivity.java
+++ app/src/main/java/com/serena/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package com.serena.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void bmi(View view){
+//          System.out.println("what???");
+        Log.d("MainActivity","testing bmi method");
+        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
+        EditText edHeight = (EditText) findViewById(R.id.ed_height);
+//        String s = edWeight.getText().toString();
+//        float weight = Float.parseFloat(s);
+        float weight = Float.parseFloat(edWeight.getText().toString());
+        float height = Float.parseFloat(edHeight.getText().toString());
+        float bmi = weight / (height*height);
+        Log.d("MainActivity","Your BMI is:"+bmi);
+
+
+
     }
 }
